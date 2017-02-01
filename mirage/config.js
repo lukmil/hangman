d96f6c07daf2eee36599e34993f56ed133f0af9d
lukmil <lmilasevicius@gmail.com>
2017-02-01 14:49:58 +0200

    Acceptance test su error

--- mirage/config.js
+++ mirage/config.js
@@ -1,8 +1,8 @@
 export default function () {
   this.timing = 500; // default
-  this.get('/words', {"errors": [{"status": "400", "title": "No words", "source": {"pointer": null}}]}, 400);
+  //this.get('/words', {"errors": [{"status": "400", "title": "No words", "source": {"pointer": null}}]}, 400);
 
-  this.get('/words2', function (schema) {
+  this.get('/words', function (schema) {
     var words = schema.words.where({});
     var random = Math.floor((Math.random() * words.length) + 1);
     return words.models[random - 1];
--- tests/acceptance/application-test.js
+++ tests/acceptance/application-test.js
@@ -1,30 +1,18 @@
-import {test} from 'qunit';
-
-import moduleForAcceptance from 'ember-hangman/tests/helpers/module-for-acceptance';
-//import game from '../pages/hangman';
-import home from '../pages/home';
-
+import {test} from "qunit";
+import moduleForAcceptance from "ember-hangman/tests/helpers/module-for-acceptance";
+import home from "../pages/home";
 moduleForAcceptance('Acceptance | application');
-// test('visiting /application', function (assert) {
-//   server.create('word', {title: "as"});
-//   home.visit();
-//
-//   andThen(function () {
-//     assert.equal(currentURL(), '/');
-//     // game.makeGuess("L");
-//   // }).andThen(function () {
-//   //   assert.equal(game.guessedLetters, "Spėtos raidės: L");
-//   //   assert.equal(game.gameStartText, "Žaidimas vyksta");
-//     assert.equal(home.errorNotification , "Klaidutė");
-//   });
-// });
 test('show error if no data given', function (assert) {
-  server.get('/words', {"errors": [{"status": "400", "title": "No words", "source": {"pointer": null}}]}, 400);
+  let adapterException = Ember.Test.adapter.exception;
+  Ember.Test.adapter.exception = (error) => server.get('/words', {"errors": ["there was an error"]}, 400);
+
+  //   assert.ok(dom.hasOne('error-message'), 'has error message');
+  //   Ember.Test.adapter.exception = adapterException;
+
+  server.get('/words', {"errors": ["there was an error"]}, 400);
   home.visit();
   andThen(() => {
-    assert.equal(home.errorNotification, ('error'));
+    assert.ok(home.errorNotification, "there is error");
+    return Ember.Test.adapter.exception = adapterException;
   });
-
-  //
-
 });
